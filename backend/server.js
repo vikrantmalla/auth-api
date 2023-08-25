6b19aa93d805b5b4c2a5c33c6da7b1eb9cc775c4
bikrant malla <bikrant.malla@truenary.com>
2023-08-25 21:19:19 +0545

    feat: organised folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import dotenv  from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
 
 // Load environment variables from a .env file into process.env.
 dotenv.config();
@@ -9,7 +11,8 @@ const port = process.env.PORT || 5000;
 // Create an instance of the Express application.
 const app = express();
 
-app.get('/', (req, res) => res.send('Server is ready'));
+app.use("/api/users", userRoutes);
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.get("/", (req, res) => res.send("Server is ready"));
 
+app.listen(port, () => console.log(`Server started on port ${port}`));
