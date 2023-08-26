923c613757ded14adaa34a450cebd97392d55e99
bikrant malla <bikrant.malla@truenary.com>
2023-08-26 12:44:00 +0545

    feat: add a custom error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import asyncHandler from "express-async-handler"
+import asyncHandler from "express-async-handler";
 
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "hello" });
---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,25 @@
+// Middleware function for handling requests to routes that do not exist (404 - Not Found)
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// Middleware function for handling errors in the application
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // If Mongoose not found error, set to 404 and change message
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
