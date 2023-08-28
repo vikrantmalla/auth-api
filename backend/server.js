7a0c4980ff7566758c919e86461dbe8f6ee82cb2
bikrant malla <bikrant.malla@truenary.com>
2023-08-28 20:46:47 +0545

    feat: setup a mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 // Load environment variables from a .env file into process.env.
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 // Create an instance of the Express application.
 const app = express();
