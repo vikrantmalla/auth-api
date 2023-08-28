0f45db6bcd3b3cc4677e92dde1e5fa5105b29d9b
bikrant malla <bikrant.malla@truenary.com>
2023-08-28 21:20:36 +0545

    feat: register a user in controller

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ connectDB();
 // Create an instance of the Express application.
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
