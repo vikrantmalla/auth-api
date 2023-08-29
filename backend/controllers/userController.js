e511ea82364e70f13af710b462d1a3ba41fa9608
bikrant malla <bikrant.malla@truenary.com>
2023-08-29 21:48:07 +0545

    fix: auth user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,22 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "hello" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Register a new user
