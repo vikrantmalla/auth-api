4784c2a158224f6dea027f8b77087e834ac745dd
bikrant malla <bikrant.malla@truenary.com>
2023-08-29 22:04:46 +0545

    feat: after logout jwt delete from cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,11 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 export {
