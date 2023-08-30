1220d80772f2850d6a38c375f899de563b69447e
bikrant malla <bikrant.malla@truenary.com>
2023-08-30 13:55:58 +0545

    feat: get user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,18 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update user profile
