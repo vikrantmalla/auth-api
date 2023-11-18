10b21dbc4aa8a618c12e2500311cf63e648f1235
bikrant malla <bikrant.malla@truenary.com>
2023-11-18 21:38:52 +0545

    feat: add a task controller handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,8 +1,31 @@
 import asyncHandler from "express-async-handler";
 
-
+// @desc    Get Task
+// @route   GET /api/tasks
+// @access  Private
 const getTasks = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "this is task route" });
 });
 
-export { getTasks };
+// @desc    Set Task
+// @route   POST /api/tasks
+// @access  Private
+const setTask = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "this is task route" });
+});
+
+// @desc    Update Task
+// @route   PUT /api/tasks/:id
+// @access  Private
+const updateTask = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "this is task route" });
+});
+
+// @desc    Delete Task
+// @route   DELETE /api/tasks/:id
+// @access  Private
+const deleteTask = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "this is task route" });
+});
+
+export { getTasks, setTask, updateTask, deleteTask };
