8aa56829db56652eea4dee5f6dd9d055fe61172c
bikrant malla <bikrant.malla@truenary.com>
2023-11-19 12:40:51 +0545

    fix: response message

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -11,21 +11,25 @@ const getTasks = asyncHandler(async (req, res) => {
 // @route   POST /api/tasks
 // @access  Private
 const setTask = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "this is task route" });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  res.status(200).json({ message: "Set task" });
 });
 
 // @desc    Update Task
 // @route   PUT /api/tasks/:id
 // @access  Private
 const updateTask = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "this is task route" });
+  res.status(200).json({ message: "Delete task" });
 });
 
 // @desc    Delete Task
 // @route   DELETE /api/tasks/:id
 // @access  Private
 const deleteTask = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "this is task route" });
+  res.status(200).json({ message: "Update task" });
 });
 
 export { getTasks, setTask, updateTask, deleteTask };
